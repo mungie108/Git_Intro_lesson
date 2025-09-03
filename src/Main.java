0640f311c0701a573884553f80c60a5bfa7f936f
ethan <ethaneshom@gmail.com>
2025-09-03 14:23:25 -0700

    making conflict

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@
             public static void main(String[] args) {
                 System.out.println("GitHub!");
                 System.out.println("Intro!");
+                System.out.println("conflict this");
             }
         }
